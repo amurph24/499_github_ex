27c4d3d6fdfeb7259e2e3b5e90740bbafd440791
amurph24 <amurph@student.ubc.ca>
2022-09-15 21:19:56 -0700

    added IntSort and corresponding test

--- src/pkg1/ObjectList.java
+++ src/pkg1/ObjectList.java
@@ -20,6 +20,9 @@ public class ObjectList<E> {
 		int newSize = elements.length*2;
 		elements = Arrays.copyOf(elements, newSize);
 	}
+	private void set(int n,E e) {
+		elements[n] = e;
+	}
 	
 	@SuppressWarnings("unchecked")
 	public E get(int i) {
@@ -28,5 +31,24 @@ public class ObjectList<E> {
 			throw new IndexOutOfBoundsException("");
 		}
 		return (E) elements[i];
+	}
+	
+	public void IntSort() {
+		
+			E Temp;
+			for (int i=0;i<size-1;++i){
+
+	            for(int j=0;j<size-i-1; ++j){
+
+	                if( (Integer) get(j+1) < (Integer) get(j)){
+
+	                    Temp = get(j);
+	                    set(j, get(j+1));
+	                    set(j+1, Temp);
+
+	                }
+	            }
+	        }
+		
 	}
 }
